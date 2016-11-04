d83129abff2e4fb82cdfb93040afb7364a24dd67
Tomisin <tomisinjenrola@gmail.com>
2016-11-04 13:25:01 -0230

    Corrected syntax and logic

--- js/map.js
+++ js/map.js
@@ -1,38 +1,12 @@
-$(function () {
+function initMap() {
+	var location = new google.maps.LatLng(47.5716807,-52.7354585);
 
-	function initMap() {
-		var location = new google.maps.LatLng(47.5737975,-52.7329053);
-
-		var mapCanvas = document.getElementById('map');
-		var mapOptions = {
-			center: location,
-			zoom: 10,
-			panControl: false,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-		var map = new google.maps.Map(mapCanvas, mapOptions);
+	var mapCanvas = document.getElementById('map');
+	var mapOptions = {
+		center: location,
+		zoom: 10,
+		panControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-
-	// google.maps.event.addDomListener(window, 'load', initMap);
-
-});
-
-// $(function () {
-
-//     function initMap() {
-
-//         var location = new google.maps.LatLng(50.0875726, 14.4189987);
-
-//         var mapCanvas = document.getElementById('map');
-//         var mapOptions = {
-//             center: location,
-//             zoom: 16,
-//             panControl: false,
-//             mapTypeId: google.maps.MapTypeId.ROADMAP
-//         }
-//         var map = new google.maps.Map(mapCanvas, mapOptions);
-
-//     }
-
-//     google.maps.event.addDomListener(window, 'load', initMap);
-// });
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+}
