3caef314dc8584dfb82fd382d8b6de408e0e2513
Tomisin <tomisinjenrola@gmail.com>
2016-11-04 13:44:35 -0230

    Added info window for location

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 function initMap() {
+
 	var location = new google.maps.LatLng(47.5716807,-52.7354585);
 
 	var mapCanvas = document.getElementById('map');
@@ -16,4 +17,17 @@ function initMap() {
 		map: map,
 		icon: markerImage
 	});
+
+	var infoText = '<div class="info-window">' +
+										'<h3>Info Window Content</h3>' +
+										'<div class="info-content">' +
+											'<p>Pellentesque habitant morbi tristique senectus et netus et malesuada.</p>' +
+										'</div>' +
+									'</div>';
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: infoText,
+		maxWidth: 400
+	});
+
 }
