21bc43600c60e1131b35682a0f243b7bfcff9b3e
Tomisin <tomisinjenrola@gmail.com>
2016-12-16 10:06:54 -0330

    Created header file for navbar

---
+++ header.php
@@ -0,0 +1,37 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+				<div class="navbar-header">
+						<span class="pull-left navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="margin-left: 10px"><i class="glyphicon glyphicon-menu-hamburger"></i></span>
+						<a class="navbar-brand" href="#">MUN Conferences App</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Home</a></li>
+								<li class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" href="#">Explore
+										<span class="caret"></span>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="campus-map.php">Campus</a></li>
+										<li><a href="#">City</a></li>
+									</ul>
+								</li>
+								<li class="dropdown">
+										<a class="dropdown-toggle" data-toggle="dropdown" href="#">Event Details
+												<span class="caret"></span>
+										</a>
+										<ul class="dropdown-menu">
+												<li><a href="event-info.php">About</a></li>
+												<li><a href="attendees.php">Guests</a></li>
+												<li><a href="sponsors.php">Sponsors</a></li>
+										</ul>
+								</li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right" id="userAccess">
+								<li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+								<li><a href="signin.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+						</ul>
+				</div>
+		</div>
+</nav>
