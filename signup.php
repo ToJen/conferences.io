8b42de64bb1d5f4c623c11011c1966d943faa2c1
Kelwin Joanes <kjoanes@gmail.com>
2017-03-01 13:49:01 -0330

    Updated the sign up page

--- signup.php
+++ signup.php
@@ -7,8 +7,28 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<!-- Bootstrap Date-picker Plugin -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+		<script>
+		    $(document).ready(function(){
+		      var date_input=$('input[name="date"]');
+		      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+		      var options={
+		        format: 'mm/dd/yyyy',
+		        container: container,
+		        todayHighlight: true,
+		        autoclose: true,
+		      };
+		      date_input.datepicker(options);
+		    })
+		</script>
 	</head>
 	<body>
+	<?php include('header.php'); ?>
+	<br />
+	<br />
     <div class="container">    
    
         <div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
@@ -23,14 +43,7 @@
 	                    <p>Error:</p>
 	                    <span></span>
 	                </div>
-	                                              
-	                <div class="form-group">
-	                    <label for="email" class="col-md-3 control-label">Email</label>
-	                    <div class="col-md-9">
-	                      <input type="text" class="form-control" name="email" placeholder="Email Address" required>
-	                    </div>
-	                </div>
-	                    
+	                
 	                <div class="form-group">
 	                    <label for="firstname" class="col-md-3 control-label">First Name</label>
 	                    <div class="col-md-9">
@@ -43,17 +56,33 @@
 	                      <input type="text" class="form-control" name="lastname" placeholder="Last Name" required>
 	                    </div>
 	                </div>
+
+	                <div class="form-group">
+	                    <label for="email" class="col-md-3 control-label">Email</label>
+	                    <div class="col-md-9">
+	                      <input type="text" class="form-control" name="email" placeholder="Email Address" required>
+	                    </div>
+	                </div>
+
 	                <div class="form-group">
 	                    <label for="password" class="col-md-3 control-label">Password</label>
 	                    <div class="col-md-9">
 	                      <input type="password" class="form-control" name="passwd" placeholder="Password" required>
 	                    </div>
 	                </div>
-	                    
+
 	                <div class="form-group">
-	                    <label for="icode" class="col-md-3 control-label">Invitation Code</label>
+	                    <label for="birthday" class="col-md-3 control-label">Birthday</label>
 	                    <div class="col-md-9">
-	                      <input type="text" class="form-control" name="icode" placeholder="" required>
+	                      <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
+	                    </div>
+	                </div>
+
+	                <div class="form-group">
+	                    <label for="sex" class="col-md-3 control-label">Sex</label>
+	                    <div class="col-md-9">
+	                      <input type="radio" class="form-horizontal" name="sex" value="male"> M
+	                      <input type="radio" class="form-horizontal" name="sex" value="female"> F
 	                    </div>
 	                </div>
 
