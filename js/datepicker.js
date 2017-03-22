a9f75b36d685228548e01ab3185c0901121a8479
Kelwin Joanes <kjoanes@gmail.com>
2017-03-22 12:40:48 -0230

    Changed date format of datepicker to dd/mm/yyyy

--- js/datepicker.js
+++ js/datepicker.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var date_input=$('input[name="date"]');
 	var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
 	var options={
-	    format: 'mm/dd/yyyy',
+	    format: 'dd/mm/yyyy',
 	    container: container,
 	    todayHighlight: true,
 	    autoclose: true,
