5a04af9f6ee6243a9368b14f4f29b3a3a02b9090
Kelwin Joanes <kjoanes@gmail.com>
2017-03-22 12:28:44 -0230

    While editing profile, email does not show as duplicate now

--- validate.php
+++ validate.php
@@ -1,24 +1,40 @@
 <?php
+	// start session
+	session_start();
+
+	// retrieve user id
+	$uid = $_SESSION["uid"];
+
 	// variables for sql connection
 	$servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "mun_conferences";
+	$username = "root";
+	$password = "";
+	$database = "mun_conferences";
 
-    // establish sql connection
-  	$conn = new mysqli($servername, $username, $password, $database);
+	// establish sql connection
+	$conn = new mysqli($servername, $username, $password, $database);
 
-  	// get email
-  	$email = $_POST["email"];
+	// get email
+	$email = $_POST["email"];
 
 	// check if user is already registered
-  	$sql = "SELECT userID FROM User WHERE eMail='$email'";
+	$sql = "SELECT userID FROM User WHERE eMail='$email'";
 
-    // run and save results of query
-    $result = $conn->query($sql);
+	// run and save results of query
+	$result = $conn->query($sql);
 
-    if($result->num_rows == 1)
-    	echo "true";
-    else
-    	echo "false";
+	if($result->num_rows == 1)
+	{
+		// fetch required fields
+        while($row = $result->fetch_assoc()) 
+        {
+			// check if email is same as signed in user
+			if($row["userID"] === $uid)
+				echo "false";
+			else
+				echo "true";
+		}
+	}
+	else
+		echo "false";
 ?>
