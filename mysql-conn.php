8400f239824bd9702783a9c0c7635da62836cd5e
Kelwin Joanes <kjoanes@gmail.com>
2017-04-18 21:18:24 -0230

    Moved database connection code to single file and referenced in all files that required it

---
+++ mysql-conn.php
@@ -0,0 +1,10 @@
+<?php
+	// variables for sql connection
+  	$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "mun_conferences";
+
+    // establish sql connection
+  	$conn = new mysqli($servername, $username, $password, $database);
+?>
