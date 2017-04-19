82bcb52a126ab5a5668789eaeb1b39feebceaa5c
Kelwin Joanes <kjoanes@gmail.com>
2017-04-19 11:56:04 -0230

    Added functionality of changing profile picture from profile page directly

--- profile.php
+++ profile.php
@@ -61,22 +61,60 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" />
 		<link rel="stylesheet" href="css/profile.css" />
+		<link rel="stylesheet" href="jquery.imgareaselect-0.9.10/css/imgareaselect-default.css" />
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="jquery.imgareaselect-0.9.10/scripts/jquery.imgareaselect.min.js"></script>
+		<script type="text/javascript" src="js/jquery.form.min.js"></script>
+		<script type="text/javascript" src="js/crop-save.js"></script>
 	</head>
 
 	<body>
 	    <?php include("header.php") ?>
 		<br>
 	    <br>
+
+	    <!-- Modal dialog for image upload -->
+		<div id="profile-pic-modal" class="modal fade" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h3 class="modal-title">Update Profile Picture</h3>
+					</div>
+					<div class="modal-body">
+						<form id="crop-image" method="post" enctype="multipart/form-data" action="">
+							<strong>Upload Image:</strong>
+							<br>
+							<br>
+							<input type="file"   name="profile-pic" id="profile-pic" />
+							<input type="hidden" name="hdn-x1-axis" id="hdn-x1-axis" value="" />
+							<input type="hidden" name="hdn-y1-axis" id="hdn-y1-axis" value="" />
+							<input type="hidden" name="hdn-x2-axis" value="" id="hdn-x2-axis" />
+							<input type="hidden" name="hdn-y2-axis" value="" id="hdn-y2-axis" />
+							<input type="hidden" name="hdn-thumb-width" id="hdn-thumb-width" value="" />
+							<input type="hidden" name="hdn-thumb-height" id="hdn-thumb-height" value="" />
+							<input type="hidden" name="action" value="" id="action" />
+							<input type="hidden" name="image_name" value="" id="image_name" />
+							<div id="preview-profile-pic"></div>
+							<div id="thumbs" style="padding:5px; width:600px"></div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button id="close-crop" type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<button id="save-crop" type="button" class="btn btn-primary">Crop &amp; Save</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
         <div class="row" style="background-color: #00B398;">
         	<div class="col-xs-12 text-center">
         		<div class="edit-box">
         			<div class="edit"><a href="edit-profile.php"><i class="glyphicon glyphicon-pencil"></i></a></div>
         		</div>
         		<div class=" profile-pic">
-	        		<img class="img-circle"
-	        		<?php if($pic === NULL) echo "src=\"imgs/profile.png\""; else echo "src=\"$pic\""; ?> width="100px" height="100px">
+	        		<a href="#" id="update-pic" data-toggle="modal" data-target="#profile-pic-modal"><img class="img-circle" id="profile-picture" <?php if($pic === NULL) echo "src=\"imgs/profile.png\""; else echo "src=\"$pic\""; ?> width="100px" height="100px"></a>
         		</div>
         		<h4 style="color: white;"><?php echo "$fname $lname"; ?></h4>
         		<h5 style="color: white;">
